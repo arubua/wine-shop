87e1bde0c97a9e463c44379c8feef374458c1687
arubua <rubua31@gmail.com>
2023-10-10 19:10:44 +0300

    fixed cart totals and delete itemformcart

--- src/components/WineItem.js
+++ src/components/WineItem.js
@@ -1,151 +1,150 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
-  Box,
-  HStack,
-  Text,
-  VStack,
-  Button,
-  Center,
-  StackDivider,
-  Image,
-  Input,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-} from "@chakra-ui/react";
-import Link from "next/link";
+	Box,
+	HStack,
+	Text,
+	VStack,
+	Button,
+	Center,
+	StackDivider,
+	Image,
+	Input,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	NumberIncrementStepper,
+	NumberDecrementStepper,
+} from '@chakra-ui/react'
+import Link from 'next/link'
 
-const WineItem = (props) => {
-  const { item, quantity, setQuantity, handleChange, addToCart } = props;
-  return (
-    <Box minW="320px">
-      <HStack>
-        <Center w="80px" h="300px">
-          <Image
-            h="280px"
-            src={`https://storage.googleapis.com/wineshop-assets/wine-bottles/${item.image} `}
-          />
-        </Center>
+const WineItem = props => {
 
-        <VStack>
-          <Center mb=".5em">
-            <VStack m="0">
-              <Text
-                lineHeight="1em"
-                fontSize="6em"
-                fontWeight="bold"
-                color="gray.400"
-              >
-                {item.no}
-              </Text>
-              <Text
-                lineHeight="1em"
-                mt="0"
-                fontSize="2em"
-                fontWeight="bold"
-                color="gray.400"
-              >
-                {item.name.substring(0, 10)}
-              </Text>
-            </VStack>
-          </Center>
-          <Center>
-            <VStack>
-              <Box>
-                <HStack p=".2em">
-                  <Box pr="8px" m=".2em" borderRight="2px" borderStyle="dotted">
-                    <Box>Bottle</Box>
-                    <Box>${item.cost.bottle}</Box>
-                    <Box>
-                      <HStack>
-                        <NumberInput
-                          min={0}
-                          name="cases"
-                          value={quantity.bottles}
-                          onChange={(e) => {
-                            setQuantity({ ...quantity, bottles: e });
-                          }}
-                        >
-                          <NumberInputField
-                            p="0"
-                            w="4em"
-                            h="3em"
-                            rounded="0"
-                            name="cases"
-                            borderColor="black"
-                          />
-                          <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                          </NumberInputStepper>
-                        </NumberInput>
+  console.log("props",props)
+	const { item, quantity, handleChange, addToCart } = props
 
-                        <Text>QTY</Text>
-                      </HStack>
-                    </Box>
-                  </Box>
-                  <Box m=".2em">
-                    <Box>Case</Box>
-                    <Box>${item.cost.case}</Box>
-                    <Box>
-                      <HStack>
-                        <NumberInput
-                          min={0}
-                          name="cases"
-                          value={quantity.cases}
-                          onChange={(e) => {
-                            setQuantity({ ...quantity, cases: e });
-                          }}
-                        >
-                          <NumberInputField
-                            p="0"
-                            w="4em"
-                            h="3em"
-                            rounded="0"
-                            name="cases"
-                            borderColor="black"
-                          />
-                          <NumberInputStepper>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                          </NumberInputStepper>
-                        </NumberInput>
-                        <Text>QTY</Text>
-                      </HStack>
-                    </Box>
-                  </Box>
-                </HStack>
-              </Box>
-              <Box>
-                <HStack spacing=".2em">
-                  <Button
-                    rounded="0"
-                    backgroundColor="gray.200"
-                    color="gray.800"
-                  >
-                    Details
-                  </Button>
+	return (
+		<Box minW="320px">
+			<HStack>
+				<Center w="80px" h="300px">
+					<Image
+						h="280px"
+						src={`https://storage.googleapis.com/wineshop-assets/wine-bottles/${item.image} `}
+					/>
+				</Center>
 
-                  <Button
-                    type="submit"
-                    disabled={!quantity.bottles && !quantity.cases}
-                    onClick={() => addToCart(item.name, quantity)}
-                    rounded="0"
-                    backgroundColor="black"
-                    color="white"
-                  >
-                    Add to Cart
-                  </Button>
-                </HStack>
-              </Box>
-            </VStack>
-          </Center>
-        </VStack>
-      </HStack>
-    </Box>
-  );
-};
+				<VStack>
+					<Center mb=".5em">
+						<VStack m="0">
+							<Text
+								lineHeight="1em"
+								fontSize="6em"
+								fontWeight="bold"
+								color="gray.400"
+							>
+								{item.no}
+							</Text>
+							<Text
+								lineHeight="1em"
+								mt="0"
+								fontSize="2em"
+								fontWeight="bold"
+								color="gray.400"
+							>
+								{item.name.substring(0, 10)}
+							</Text>
+						</VStack>
+					</Center>
+					<Center>
+						<VStack>
+							<Box>
+								<HStack p=".2em">
+									<Box pr="8px" m=".2em" borderRight="2px" borderStyle="dotted">
+										<Box>Bottle</Box>
+										<Box>${item.cost.bottle}</Box>
+										<Box>
+											<HStack>
+												<NumberInput
+													min={0}
+													name="bottles"
+													value={quantity?.bottles}
+													onChange={q => handleChange(q, 'bottles', item)}
+												>
+													<NumberInputField
+														p="0"
+														w="4em"
+														h="3em"
+														rounded="0"
+														name="bottles"
+														borderColor="black"
+													/>
+													<NumberInputStepper>
+														<NumberIncrementStepper />
+														<NumberDecrementStepper />
+													</NumberInputStepper>
+												</NumberInput>
 
-export default WineItem;
+												<Text>QTY</Text>
+											</HStack>
+										</Box>
+									</Box>
+									<Box m=".2em">
+										<Box>Case</Box>
+										<Box>${item.cost.case}</Box>
+										<Box>
+											<HStack>
+												<NumberInput
+													min={0}
+													name="cases"
+													value={quantity?.cases}
+													onChange={q => handleChange(q, 'cases', item)}
+												>
+													<NumberInputField
+														p="0"
+														w="4em"
+														h="3em"
+														rounded="0"
+														name="cases"
+														borderColor="black"
+													/>
+													<NumberInputStepper>
+														<NumberIncrementStepper />
+														<NumberDecrementStepper />
+													</NumberInputStepper>
+												</NumberInput>
+												<Text>QTY</Text>
+											</HStack>
+										</Box>
+									</Box>
+								</HStack>
+							</Box>
+							<Box>
+								<HStack spacing=".2em">
+									<Button
+										rounded="0"
+										backgroundColor="gray.200"
+										color="gray.800"
+									>
+										Details
+									</Button>
+
+									<Button
+										type="submit"
+										disabled={quantity?.bottles < 1 && quantity?.cases < 1}
+										onClick={() => addToCart(item, quantity)}
+										rounded="0"
+										backgroundColor="black"
+										color="white"
+									>
+										Add to Cart
+									</Button>
+								</HStack>
+							</Box>
+						</VStack>
+					</Center>
+				</VStack>
+			</HStack>
+		</Box>
+	)
+}
+
+export default WineItem
--- src/components/Wines.js
+++ src/components/Wines.js
@@ -1,79 +1,77 @@
-import React, { useEffect, useState } from "react";
-import WineItem from "./WineItem";
+import React, { useEffect, useState } from 'react'
+import WineItem from './WineItem'
 import {
-  Box,
-  VStack,
-  StackDivider,
-  Stack,
-  Spacer,
-  Flex,
-  Center,
-  Divider,
-} from "@chakra-ui/react";
+	Box,
+	VStack,
+	StackDivider,
+	Stack,
+	Spacer,
+	Flex,
+	Center,
+	Divider,
+} from '@chakra-ui/react'
 
 const Wines = ({
-  data,
-  filterState,
-  addToCart,
+	data,
+	filterState,
+	addToCart,
 
-  handleChange,
-  quantity,
-  setQuantity,
+	handleChange,
+	quantity,
+	setQuantity,
 }) => {
-  const filterTags = filterState.map((tag) => tag.toLowerCase());
+	const filterTags = filterState.map(tag => tag.toLowerCase())
 
-  return (
-    <Center maxW="2500px" w="90vw" display="flex" flexWrap="wrap">
-      {filterState.length === 0 || filterState.length > 1
-        ? data.map((item) => (
-            <Box maxW="600px" mt="1em" key={item.name}>
-              <WineItem
-                item={item}
-                addToCart={addToCart}
-                quantity={quantity}
-                setQuantity={setQuantity}
-                handleChange={handleChange}
-              />
-              <Divider m=".3em" orientation="horizontal" />
-            </Box>
-          ))
-        : filterState[0] === "price"
-        ? data
-            .sort((a, b) => (a.cost.bottle > b.cost.bottle ? 1 : -1))
-            .map((item) => (
-              <Box mt="1em" key={item.name}>
-                <WineItem
-                  item={item}
-                  addToCart={addToCart}
-                  quantity={quantity}
-                  setQuantity={setQuantity}
-                  handleChange={handleChange}
-                />
-                <Divider m=".3em" orientation="horizontal" />
-              </Box>
-            ))
-        : data.map((item) => {
-            const tags = item.tags;
+	const [cart, setCart] = useState([])
 
-            for (var i = 0; i < tags.length; i++) {
-              if (tags[i].includes(filterTags)) {
-                return (
-                  <Box mt="1em" key={item.name}>
-                    <WineItem
-                      item={item}
-                      addToCart={addToCart}
-                      quantity={quantity}
-                      setQuantity={setQuantity}
-                      handleChange={handleChange}
-                    />
-                    <Divider m=".3em" orientation="horizontal" />
-                  </Box>
-                );
-              }
-            }
-          })}
-    </Center>
-  );
-};
+	useEffect(() => {}, [])
 
-export default Wines;
+	// const handleChange = (q, type, item) => {
+	// 	console.log('q', q)
+	// 	console.log('type', type)
+
+	// 	console.log('item', item)
+	// }
+
+	return (
+		<Center maxW="2500px" w="90vw" display="flex" flexWrap="wrap">
+			{filterState[0] === 'price'
+				? data
+						.sort((a, b) => (a.cost.bottle > b.cost.bottle ? 1 : -1))
+						.map(item => (
+							<Box mt="1em" key={item.name}>
+								<WineItem
+									item={item}
+									addToCart={addToCart}
+									quantity={item.quantity}
+									setQuantity={setQuantity}
+									handleChange={handleChange}
+								/>
+								<Divider m=".3em" orientation="horizontal" />
+							</Box>
+						))
+				: data.map(item => {
+						const tags = item.tags
+
+						for (var i = 0; i < tags.length; i++) {
+							if (tags[i].includes(filterTags)) {
+								return (
+									<Box mt="1em" key={item.name}>
+										<WineItem
+											item={item}
+											addToCart={addToCart}
+											quantity={item.quantity}
+											setQuantity={setQuantity}
+											handleChange={handleChange}
+										/>
+										<Divider m=".3em" orientation="horizontal" />
+									</Box>
+								)
+							}
+						}
+				  })}
+		</Center>
+	)
+}
+
+export default Wines
